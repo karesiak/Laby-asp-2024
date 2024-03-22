6858a2bd9f4862e54d77d38ce955559eaa696bef
karesiak <kordek0945@gmail.com>
2024-03-22 22:08:09 +0100

    Add project files.

---
+++ Lab 1 2024/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Lab_1_2024.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Lab_1_2024.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Calculator()
+        {
+            string op = Request.Query["op"];
+            ViewBag.Op = op;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
